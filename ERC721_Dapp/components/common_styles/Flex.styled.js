c4b1ef23b6c1dbd472b5d04e212cd1e42c3b844d
mbuh divine <mbuhdivine_cho7@yahoo.com>
2022-07-08 19:28:04 +0100

    Added styling to the flex

--- ERC721_Dapp/components/common_styles/Flex.styled.js
+++ ERC721_Dapp/components/common_styles/Flex.styled.js
@@ -3,10 +3,11 @@ import styled from "styled-components";
 export const Flex = styled.div`
     display: flex;
     align-items: center;
+    flex-wrap: wrap;
 
     & > div,
     & > ul {
-        flex: 1;
+        flex: 1 0 21%;
     }
 
     @media(max-width: ${({theme}) => theme.mobile}) {
